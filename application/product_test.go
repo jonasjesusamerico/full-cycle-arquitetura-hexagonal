63d7a179946879cf42be400a2971c7c28a75ec6e
Jonas de Jesus Américo <jjsamerico@gmail.com>
2023-02-15 20:35:40 -0300

    criado teste e atualizado docker-compos

--- application/product_test.go
+++ application/product_test.go
@@ -1,1 +1,24 @@
-package application
+package application_test
+
+import (
+	"github.com/jonasjesusamerico/full-cycle-arquitetura-hexagonal/application"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+
+	require.Nil(t, err)
+
+	product.Price = 0
+
+	err = product.Enable()
+
+	require.Equal(t, "the price most be greater than zero to enable the product", err.Error())
+}
